2ddcf1e391c339522c5771baaf77493f013812a2
Surajit <surajitjana20107@gmail.com>
2026-02-27 18:38:18 +0000

    set the cache mode to no-store in http methods

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,7 @@ export const apiFetch = async (url: string, method: string, body?: any) => {
         "Content-Type": "application/json",
       },
       credentials: "include", // important for cookies!
+      cache: "no-store",
     };
 
     if (body) {
